5345d381fb1ba3166a95cead618256c1a2c240bf
Brittany <brterbush@zappos.com>
2021-04-30 14:04:51 -0700

    Format blog text.

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -3,6 +3,7 @@ import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 import DeleteIcon from "@material-ui/icons/Delete"
 import { makeStyles } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
 import "./Post.css"
 
 const useStyles = makeStyles((theme) => ({
@@ -24,9 +25,11 @@ const Post = ({ title, content, editPost, deletePost, id, user, image }) => {
                 <Typography variant="h5" gutterBottom>
                     {title}
                 </Typography>
-                <Typography variant="body2" gutterBottom>
-                    {content}
-                </Typography>
+                <Container maxWidth="sm">
+                    <Typography variant="body2" gutterBottom>
+                        {content}
+                    </Typography>
+                </Container>
                 <Typography variant="body2" gutterBottom>
                     Author: {user}
                 </Typography>
